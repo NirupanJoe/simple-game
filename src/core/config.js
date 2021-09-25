8efa2ee963e1298c0ca962f0141fcb128b634dd8
SivaHariG <glitzsiva98@gmail.com>
2021-09-25 06:20:01 +0000

    config add Targets

--- src/core/config.js
+++ src/core/config.js
@@ -5,6 +5,21 @@ const config = {
 	bgnScreenYIncre: 1,
 	width: 83,
 	maxTargets: 5,
+	targets: {
+		shooter: {
+			health: 1,
+			damage: 1,
+			type: 'shooter',
+			// eslint-disable-next-line max-len
+			image: 'https://i0.wp.com/apkdeal.com/wp-content/uploads/2017/12/crazy-plane-flight-shooting-game-game-apk-download-for-free-in-your-android-ios.jpg?fit=600%2C600&ssl=1&resize=350%2C200',
+			height: 10,
+			width: 20,
+			variance: 0.2,
+			prop: {
+				spawn: 1,
+			},
+		},
+	},
 };
 
 export default config;

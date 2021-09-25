5c748ced70f078ebb05d1951d399f01579242c67
SivaHariG <glitzsiva98@gmail.com>
2021-09-25 06:21:36 +0000

    position services introduced.

--- src/services/positionService.js
+++ src/services/positionService.js
@@ -1,13 +1,18 @@
 import config from '../core/config';
+import { rndBetween } from '@laufire/utils/random';
 
 const { width } = config;
-// const two = 2;
+const hundred = 100;
+const two = 2;
 
 const project = (data) =>
 	Math.max(data - width, 0);
 
+const getRandomValue = (data) =>
+	rndBetween(data / two, hundred - (data / two));
 const PositionService = {
 	project,
+	getRandomValue,
 };
 
 export default PositionService;

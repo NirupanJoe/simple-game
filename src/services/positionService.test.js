e0b7d64755e45a002ae6c34e0f617029bfce3d73
SivaHariG <glitzsiva98@gmail.com>
2021-09-25 06:22:38 +0000

    position services test introduced.

--- src/services/positionService.test.js
+++ src/services/positionService.test.js
@@ -1,7 +1,14 @@
-import PositionService from './positionService';
+/* eslint-disable max-lines-per-function */
 
-describe('testing PositionService', () => {
-	const { project } = PositionService;
+import * as random from '@laufire/utils/random';
+import positionService from './positionService';
+
+describe('PositionService', () => {
+	const { project, getRandomValue } = positionService;
+	const twentyFive = 25;
+	const hundred = 100;
+	const two = 2;
+	const range = random.rndBetween(twentyFive, hundred);
 
 	test('project returns value greater than or equal to 0', () => {
 		let i = 0;
@@ -14,4 +21,19 @@ describe('testing PositionService', () => {
 			expect(result).toBeGreaterThanOrEqual(0);
 		}
 	});
+
+	test('get random value for height and width',
+		() => {
+			const data = range;
+			const min = range / two;
+			const max = hundred - min;
+			const returnValue = Symbol('returnValue');
+
+			jest.spyOn(random, 'rndBetween').mockReturnValue(returnValue);
+
+			const result = getRandomValue(data);
+
+			expect(random.rndBetween).toHaveBeenCalledWith(min, max);
+			expect(result).toEqual(returnValue);
+		});
 });

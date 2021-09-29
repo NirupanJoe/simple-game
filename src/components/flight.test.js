9353f66cc965c3788f48c2272ba46c53fba5bd31
Thanika_Somasundaram_T <thanikasomasundaram1801@gmail.com>
2021-09-29 20:22:05 +0530

    Refactored Flight test.

--- src/components/flight.test.js
+++ src/components/flight.test.js
@@ -1,5 +1,6 @@
 import Flight from './flight';
 import { render } from '@testing-library/react';
+import context from '../core/context';
 
 jest.mock('../core/context', () => ({
 	state: { flight: { x: 100 }},
@@ -11,5 +12,8 @@ describe('testing Flight', () => {
 
 		expect(component).toBeInTheDocument();
 		expect(component).toHaveClass('flight');
+		expect(component).toHaveStyle({
+			left: `${ context.state.flight.x }px`,
+		});
 	});
 });

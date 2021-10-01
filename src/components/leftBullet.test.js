0deee869acbd83439998a91dbeb38213215adf2e
somukhannan <somukhannan8@gmail.com>
2021-10-01 13:59:52 +0000

    Refractor Left Bullet Test.

--- src/components/leftBullet.test.js
+++ src/components/leftBullet.test.js
@@ -1,10 +1,11 @@
 import { render } from '@testing-library/react';
 import leftBullet from './leftBullet';
 
-describe('score card', () => {
-	test('to Check score card', () => {
+describe('Left Bullet', () => {
+	test('to display Left Bullet', () => {
 		const component = render(leftBullet()).getByRole('leftBullet');
 
+		expect(component).toBeInTheDocument();
 		expect(component).toHaveClass('leftBullet');
 	});
 });

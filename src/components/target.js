ebc9b7ec38964670f74a2296a42d063dfe715aa1
SivaHariG <glitzsiva98@gmail.com>
2021-10-04 13:59:15 +0000

    Target component  introduced.

---
+++ src/components/target.js
@@ -0,0 +1,24 @@
+import { React } from 'react';
+
+const Target = (target) => {
+	const { id, height, width, x, y, image } = target;
+	const two = 2;
+	const style = {
+		position: 'absolute',
+		left: `${ x - (width / two) }%`,
+		top: `${ y - (height / two) }%`,
+		height: `${ height }vw`,
+		width: `${ width }vw`,
+	};
+
+	return (
+		<img
+			key={ id }
+			role="targets"
+			style={ style }
+			src={ image }
+		/>
+	);
+};
+
+export default Target;
---
+++ src/components/target.test.js
@@ -0,0 +1,30 @@
+/* eslint-disable max-lines-per-function */
+import { render } from '@testing-library/react';
+import Target from './target';
+
+describe('Target', () => {
+	const two = 2;
+
+	test('renders the component with appropriate styling', () => {
+		const target = {
+			x: 10,
+			y: 15,
+			width: 20,
+			height: 25,
+		};
+		const { x, y, width, height } = target;
+
+		const { getByRole } = render(Target(target));
+
+		const component = getByRole('targets');
+
+		expect(component).toBeInTheDocument();
+		expect(component).toHaveStyle({
+			position: 'absolute',
+			top: `${ y - (height / two) }%`,
+			left: `${ x - (width / two) }%`,
+			height: `${ height }vw`,
+			width: `${ width }vw`,
+		});
+	});
+});

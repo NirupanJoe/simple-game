670b2d9a1a13c9ad0e920d462d8a208fa0b3a021
NirupanJoe <nirupanjoej@gmail.com>
2021-10-07 12:33:21 +0000

    Refactored.

--- src/core/config.js
+++ src/core/config.js
@@ -1,3 +1,5 @@
+import fighterJet from '../images/fighterJet.png';
+
 const config = {
 	tickerDelay: 1000,
 	health: 100,
@@ -9,10 +11,9 @@ const config = {
 			health: 1,
 			damage: 1,
 			type: 'shooter',
-			// eslint-disable-next-line max-len
-			image: 'https://i0.wp.com/apkdeal.com/wp-content/uploads/2017/12/crazy-plane-flight-shooting-game-game-apk-download-for-free-in-your-android-ios.jpg?fit=600%2C600&ssl=1&resize=350%2C200',
-			height: 10,
-			width: 20,
+			image: fighterJet,
+			height: 6,
+			width: 6,
 			variance: 0.2,
 			prop: {
 				spawn: 1,

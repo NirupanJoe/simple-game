7c528405499d7834984b9cda274a944fc17b0905
somukhannan <somukhannan8@gmail.com>
2021-10-09 05:51:22 +0000

    Add Cloud Objects to seeds.

--- src/core/seed.js
+++ src/core/seed.js
@@ -3,12 +3,22 @@ import config from './config';
 const seed = {
 	health: config.health,
 	score: 0,
-	bgnScreenY: 0,
+	bgnScreenY: 10,
 	flight: {
 		x: 0,
 		width: 6,
 	},
 	targets: [],
+	objects: [{
+		x: 20,
+		y: -20,
+		type: 'Cloud',
+	},
+	{
+		x: 50,
+		y: -20,
+		type: 'Cloud',
+	}],
 };
 
 export default seed;

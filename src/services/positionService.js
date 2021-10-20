7c4246559af8c4cda005e06f5829f5df45518e84
somukhannan <somukhannan8@gmail.com>
2021-10-20 14:26:10 +0000

    Restructured the Position service.

--- src/services/positionService.js
+++ src/services/positionService.js
@@ -3,24 +3,21 @@ import { rndBetween } from '@laufire/utils/random';
 const hundred = 100;
 const two = 2;
 
-const project = (x, width) =>
-	Math.min(hundred - width, Math
-		.max(x - (width / two), 0));
-
-const pxToPercentage = (xPos, innerWidth) =>
-	xPos / innerWidth * hundred;
-
-const bulletPos = ({ state, config }) =>
-	(state.flight.width / two) + state.flight.x - (config.bullet.width / two);
-
-const getRandomValue = (data) =>
-	rndBetween(data / two, hundred - (data / two));
-
 const PositionService = {
-	project,
-	bulletPos,
-	getRandomValue,
-	pxToPercentage,
+	project: (xPos, width) =>
+		Math.min(hundred - width, Math
+			.max(xPos - (width / two), 0)),
+
+	pxToPercentage: (xPos, innerWidth) =>
+		xPos / innerWidth * hundred,
+
+	bulletPos: ({ state, config }) =>
+		(state.flight.width / two)
+		+ state.flight.x - (config.bullet.width / two),
+
+	getRandomValue: (data) =>
+		rndBetween(data / two, hundred - (data / two)),
+
 };
 
 export default PositionService;

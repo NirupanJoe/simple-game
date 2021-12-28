1012da21ecf87ada2dad7bf7e1fca73282f5ebda
NirupanJoe <nirupanjoej@gmail.com>
2021-12-28 20:01:14 +0530

    Temporary.

--- src/components/3dMode/model/flight.js
+++ src/components/3dMode/model/flight.js
@@ -1,21 +1,38 @@
+/* eslint-disable no-magic-numbers */
 import React, { useEffect } from 'react';
 import { a } from '@react-spring/three';
 import { useAnimations, useGLTF } from '@react-three/drei';
 
+// eslint-disable-next-line max-lines-per-function
 const Flight = ({ data: { rotation, position }}) => {
 	const { scene, animations } = useGLTF(`${ process.env.PUBLIC_URL }/flight/flight.gltf`);
+	const { scene: sceneOne, animations: animationsOne } = useGLTF(`${ process.env.PUBLIC_URL }/flight/flight.gltf?1`);
 	const { ref, actions } = useAnimations(animations);
+	const { ref: refOne, actions: actionsOne } = useAnimations(animationsOne);
 
 	useEffect(() => actions.default.play(), []);
+	useEffect(() => actionsOne.default.play(), []);
 	return (
-		<a.mesh
-			key={ 0 }
-			ref={ ref }
-			rotation={ rotation }
-			position={ position }
-		>
-			<primitive object={ scene } scale={ 0.8 }/>
-		</a.mesh>
+		<>
+			<group>
+				<a.primitive
+					key={ 0 }
+					ref={ ref }
+					object={ scene }
+					scale={ 0.8 }
+					rotation={ rotation }
+					position={ [0, 0, 0] }
+				/>
+			</group>
+			<group>
+				<a.primitive
+					ref={ refOne }
+					object={ sceneOne }
+					scale={ 0.8 }
+					position={ position }
+				/>
+			</group>
+		</>
 	);
 };
 

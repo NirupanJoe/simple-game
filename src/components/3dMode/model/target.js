89bddadb6a488a0e8402c2ab37dd8d4aff9b63c0
NirupanJoe <nirupanjoej@gmail.com>
2021-12-31 11:09:07 +0530

    Animated targets.

--- src/components/3dMode/model/target.js
+++ src/components/3dMode/model/target.js
@@ -1,18 +1,44 @@
-import { React } from 'react';
+import { React, useEffect, useMemo } from 'react';
 import { a } from '@react-spring/three';
+import { useAnimations, useGLTF } from '@react-three/drei';
+import { SkeletonUtils } from 'three-stdlib';
 
-const Target = ({ data: { rotation, position, id, scene }}) =>
-	<a.group
-		key={ id }
-		dispose={ null }
-		rotation={ rotation }
-		position={ position }
-	>
-		<directionalLight intensity={ 0.8 }/>
-		<primitive
-			object={ scene }
-			scale={ 0.8 }
-		/>
-	</a.group>;
+// eslint-disable-next-line max-lines-per-function
+const Model = ({ color }) => {
+	const { scene, materials, animations } = useGLTF(`${ process.env.PUBLIC_URL }/target/target.gltf`);
+	const clone = useMemo(() => SkeletonUtils.clone(scene), [scene]);
+	const { ref, actions } = useAnimations(animations);
+
+	useEffect(() => actions.default.play(), []);
+
+	return (
+		<mesh
+			ref={ ref }
+			material={ materials.flight }
+			material-color={ color }
+		>
+			<directionalLight intensity={ 0.8 }/>
+			<primitive
+				object={ clone }
+				scale={ 0.8 }
+			/>
+			<meshStandardMaterial color={ color }/>
+		</mesh>
+	);
+};
+
+const Target = ({ data }) => {
+	const { rotation, position, id } = data;
+
+	return (
+		<a.group
+			key={ id }
+			dispose={ null }
+			rotation={ rotation }
+			position={ position }
+		>
+			<Model { ...data }/>
+		</a.group>);
+};
 
 export default Target;
--- src/components/3dMode/scene/targets.js
+++ src/components/3dMode/scene/targets.js
@@ -1,15 +1,10 @@
-import { useGLTF } from '@react-three/drei';
-import { useMemo } from 'react';
-import { SkeletonUtils } from 'three-stdlib';
 import PositionService from '../../../services/positionService';
 import getSprings from '../animation';
 import TargetModel from '../model/target';
 
 const Target = (context) => {
 	const { state: { targets }} = context;
-	const { scene } = useGLTF(`${ process.env.PUBLIC_URL }/target/target.gltf`);
-	const clones = useMemo(() => targets.map(() =>
-		SkeletonUtils.clone(scene)), [targets.length]);
+
 	const enrichedTargets = targets.map((data) =>
 		PositionService.threeDProject({ ...context, data }));
 
@@ -20,7 +15,6 @@ const Target = (context) => {
 				data: {
 					...enrichedTargets[i],
 					...animationData,
-					scene: clones[i],
 				},
 			}))
 	);

3d1725a7e35e5a1635438a4a7360566eae9c9275
NirupanJoe <nirupanjoej@gmail.com>
2022-01-28 14:54:27 +0530

    Test for target.

--- src/components/3dMode/model/target/index.test.js
+++ src/components/3dMode/model/target/index.test.js
@@ -1,19 +1,30 @@
+jest.mock('@react-spring/three', () => ({
+	useSpring: jest.fn(),
+}));
+
 import React from 'react';
 import { rndBetween } from '@laufire/utils/random';
-import Target from '.';
+import Target from './index';
 import helper from '../../../../testHelper/helper';
+import * as Model from './model';
+import * as spring from '@react-spring/three';
+import animation from '../../animation/animation';
 
 test('Target', async () => {
-	const data = {
-		rotation: rndBetween(),
-		position: rndBetween(),
-		color: 'red',
-	};
-	const scale = 0.5;
+	const data = Symbol('data');
+	const props = { scale: rndBetween() };
+	const getAnimation = Symbol('getAnimation');
+	const getSpring = Symbol('getSpring');
+
+	jest.spyOn(animation, 'target').mockReturnValue(getAnimation);
+	jest.spyOn(spring, 'useSpring').mockReturnValue(getSpring);
+	jest.spyOn(Model, 'default').mockReturnValue(<mesh { ...props }/>);
 
 	const scene = await helper.getScene(<Target data={ data }/>);
-	const mesh = scene.children[0].allChildren;
+	const mesh = scene.allChildren;
 
 	expect(mesh.length).toBe(1);
-	expect(mesh[0].props.scale).toEqual(scale);
+	expect(mesh[0].props).toEqual(props);
+	expect(animation.target).toHaveBeenCalledWith(data);
+	expect(spring.useSpring).toHaveBeenCalledWith(getAnimation);
 });
